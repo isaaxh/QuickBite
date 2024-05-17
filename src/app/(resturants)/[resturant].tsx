f67ecfda07830ee5fb2aa2b175af515effb31a9c
Isaac Hussain <isaaclearns58@gmail.com>
2024-05-17 18:42:58 +0300

    refactor: seprate ResturantTopSection into seprate file

--- src/app/(resturants)/[resturant].tsx
+++ src/app/(resturants)/[resturant].tsx
@@ -2,6 +2,7 @@ import { View, Text, Image } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
 import BackButton from "@/components/BackButton";
+import ResturantTopSection from "@/components/ResturantTopSection";
 
 type TResturant = {
   id: number;
@@ -14,31 +15,9 @@ type TResturant = {
 export default function ResturantScreen() {
   const resturant = useLocalSearchParams();
 
-  const resturantImg = {
-    uri: resturant.imgUrl as string,
-    width: 450,
-    height: 250,
-  };
-
   return (
     <View className="bg-orange-300 flex-1">
-      <View className="absolute z-50 top-10 left-5">
-        <BackButton />
-      </View>
-      <View>
-        <Image source={resturantImg} />
-        <View
-          className="absolute bottom-5 left-5 rounded-xl px-6 py-2"
-          style={{ backgroundColor: "#00000088" }}
-        >
-          <Text
-            style={{ fontFamily: "Gilory-SemiBold", fontSize: 11 }}
-            className="text-white"
-          >
-            Free delivery on orders above $50
-          </Text>
-        </View>
-      </View>
+      <ResturantTopSection imgUrl={resturant.imgUrl as string} />
       <Text>{resturant.name}</Text>
     </View>
   );
---
+++ src/components/ResturantTopSection.tsx
@@ -0,0 +1,36 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import BackButton from "@/components/BackButton";
+
+type ResturantTopSectionProps = { imgUrl: string };
+
+const ResturantTopSection = ({ imgUrl }: ResturantTopSectionProps) => {
+  const resturantImg = {
+    uri: imgUrl as string,
+    width: 450,
+    height: 250,
+  };
+  return (
+    <>
+      <View className="absolute z-50 top-10 left-5">
+        <BackButton />
+      </View>
+      <View>
+        <Image source={resturantImg} />
+        <View
+          className="absolute bottom-5 left-5 rounded-xl px-6 py-2"
+          style={{ backgroundColor: "#00000088" }}
+        >
+          <Text
+            style={{ fontFamily: "Gilory-SemiBold", fontSize: 11 }}
+            className="text-white"
+          >
+            Free delivery on orders above $50
+          </Text>
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default ResturantTopSection;
