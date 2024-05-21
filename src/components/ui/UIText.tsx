3a1b91e803112e6cac42843144a2fbd7da387529
Isaac Hussain <isaaclearns58@gmail.com>
2024-05-21 12:51:27 +0300

    refactor: change color of text

--- src/components/ui/UIText.tsx
+++ src/components/ui/UIText.tsx
@@ -1,5 +1,5 @@
 import { Text } from "react-native";
-import React from "react";
+import React, { ReactNode, forwardRef } from "react";
 import { VariantProps, cva } from "class-variance-authority";
 import { TextProps } from "react-native-svg";
 import { cn } from "@/utils/cn";
@@ -18,7 +18,7 @@ import { cn } from "@/utils/cn";
 /* font - GiloryMedium; */
 
 interface UITextProps extends TextProps, VariantProps<typeof txtStyles> {
-  children: string;
+  children: ReactNode;
   textStyles?: string;
 }
 
@@ -26,10 +26,12 @@ const textVariants = {
   variant: {
     base: ["font-GiloryMedium text-base text-center"],
     header: ["font-GiloryBold text-2xl mt-4 mb-1.5"],
-    header2: ["font-GilorySemiBold text-xl mt-2"],
-    header3: ["font-GilorySemiBold text-xs mt-1"],
+    header2: ["font-GilorySemiBold text-xl"],
+    header3: ["font-GiloryBold text-base text-black"],
+    header4: ["font-GilorySemiBold text-xs"],
     body: ["font-GiloryMedium text-base text-gray-500"],
     body2: ["font-GiloryMedium text-xs text-gray-500"],
+    body3: ["font-GilorySemiBold text-xs text-gray-500"],
   },
 };
 
@@ -40,13 +42,17 @@ const txtStyles = cva(textVariants.variant.base, {
   },
 });
 
-const UIText = (Props: UITextProps) => {
+const UIText = forwardRef<Text, UITextProps>((Props: UITextProps, ref) => {
   const { children, variant, textStyles, ...props } = Props;
   return (
-    <Text className={cn(txtStyles({ variant }), textStyles)} {...props}>
+    <Text
+      ref={ref}
+      className={cn(txtStyles({ variant }), textStyles)}
+      {...props}
+    >
       {children}
     </Text>
   );
-};
+});
 
 export default UIText;
