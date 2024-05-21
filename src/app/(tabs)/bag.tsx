f71cd97b550529ac73f53e2f58eba3909cce93b9
Isaac Hussain <isaaclearns58@gmail.com>
2024-05-21 08:52:02 +0300

    feat: create a reusable button and test it

--- src/app/(tabs)/bag.tsx
+++ src/app/(tabs)/bag.tsx
@@ -1,18 +1,44 @@
-import { StyleSheet } from "react-native";
-
+import { Pressable, StyleSheet, TouchableOpacity } from "react-native";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import UIButton from "@/components/ui/UIButton";
+import { Link } from "expo-router";
 
 export default function BagScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bag</Text>
+      <Text className="font-GiloryExtraBold" style={styles.title}>
+        Bag
+      </Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
+
+      <UIButton variant="bare" size="small">
+        Text
+      </UIButton>
+      <UIButton variant="outline" size="default">
+        Outline
+      </UIButton>
+
+      <Link href={"/(tabs)/home"} asChild>
+        <UIButton variant="fill" size="large">
+          Get Started
+        </UIButton>
+      </Link>
+      <View className="mb-8 mx-5 bg-green-400">
+        <Pressable
+          android_ripple={{ color: "black", borderless: true }}
+          className="bg-primary-100 rounded-2xl py-4 items-center"
+        >
+          <Text style={{ fontFamily: "Gilory-SemiBold", fontSize: 16 }}>
+            Get Started
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -25,7 +51,6 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
--- src/components/ui/UIButton.tsx
+++ src/components/ui/UIButton.tsx
@@ -1,48 +1,76 @@
-import { Text, TouchableOpacity } from "react-native";
-import React, { forwardRef } from "react";
+import { Pressable, PressableProps, Text, View } from "react-native";
+import React, { ReactNode, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/utils/cn";
 
-type UIButtonProps = {
-  title: string;
-  color?: string;
-  variant?: "fill" | "outline";
+interface UIButtonProps extends PressableProps, VariantProps<typeof btnStyles> {
+  children: ReactNode;
+  containerStyles?: string;
+  buttonStyles?: string;
+  textStyles?: string;
+}
+
+const buttonVariants = {
+  text: {
+    baseText: "text-center font-GiloryRegular",
+    smallText: "text-xs font-GilorySemiBold",
+    defaultText: "text-base text-gray-600 font-GiloryMedium",
+    largeText: "text-base font-GilorySemiBold",
+  },
+  variant: {
+    bare: [""],
+    outline: ["border border-gray-400"],
+    fill: ["bg-primary-100"],
+  },
+  size: {
+    small: [""],
+    default: ["py-2 px-4"],
+    large: ["py-4 items-center flex-1 mx-4"],
+  },
 };
 
-const UIButton = ({ title, color, variant }: UIButtonProps) => {
-  return <Text>Go to bag</Text>;
-};
+const btnStyles = cva(["rounded-3xl"], {
+  variants: buttonVariants,
+  defaultVariants: {
+    variant: "fill",
+    size: "default",
+  },
+});
+
+const UIButton = forwardRef<View, UIButtonProps>(
+  (Props: UIButtonProps, forwardedRef) => {
+    const {
+      children,
+      variant,
+      size,
+      containerStyles,
+      buttonStyles,
+      textStyles,
+      ...props
+    } = Props;
+
+    const textKey = `${size}Text` as keyof typeof buttonVariants.text;
+    const textStyle = buttonVariants.text[textKey];
+
+    return (
+      <View className={cn("flex-row", containerStyles)}>
+        <Pressable
+          ref={forwardedRef}
+          className={cn(
+            btnStyles({ variant: variant, size: size }),
+            buttonStyles,
+          )}
+          {...props}
+        >
+          <Text
+            className={cn(buttonVariants.text.baseText, textStyle, textStyles)}
+          >
+            {children}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  },
+);
 
 export default UIButton;
-
-{
-  /* <TouchableOpacity */
-}
-{
-  /*   ref={ref} */
-}
-{
-  /*   className="bg-primary-100 rounded-2xl py-3 w-full items-center" */
-}
-{
-  /* > */
-}
-{
-  /*   <Text */
-}
-{
-  /*     className="text-black-100" */
-}
-{
-  /*     style={{ fontFamily: "Gilory-SemiBold" }} */
-}
-{
-  /*   > */
-}
-{
-  /*     {title} */
-}
-{
-  /*   </Text> */
-}
-{
-  /* </TouchableOpacity> */
-}
