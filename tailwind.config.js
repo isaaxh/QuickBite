d1a059132f0c8460384b8a2c49076ee0c483400e
Isaac Hussain <isaaclearns58@gmail.com>
2024-05-21 08:14:39 +0300

    chore: add Gilory fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        GiloryExtraBold: ["Gilory-ExtraBold"],
+        GiloryBold: ["Gilory-Bold"],
+        GilorySemiBold: ["Gilory-SemiBold"],
+        GiloryRegular: ["Gilory-Regular"],
+        GiloryMedium: ["Gilory-Medium"],
+        GiloryLight: ["Gilory-Light"],
+      },
       /*figure out how to import the COLORS var from colors.ts */
       colors: {
         primary: {
@@ -29,3 +37,10 @@ module.exports = {
   },
   plugins: [],
 };
+
+/* "Gilory-Bold": require("../../assets/fonts/Gilroy-Bold.ttf"), */
+/* "Gilory-ExtraBold": require("../../assets/fonts/Gilroy-ExtraBold.otf"), */
+/* "Gilory-SemiBold": require("../../assets/fonts/Gilroy-SemiBold.ttf"), */
+/* "Gilory-Regular": require("../../assets/fonts/Gilroy-Regular.ttf"), */
+/* "Gilory-Medium": require("../../assets/fonts/Gilroy-Medium.ttf"), */
+/* "Gilory-Light": require("../../assets/fonts/Gilroy-Light.otf"), */
