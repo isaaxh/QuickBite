c41f33aeba452103540e0d10d2b8b7aca50b1433
Isaac Hussain <isaaclearns58@gmail.com>
2024-05-22 06:12:13 +0300

    feat: add press feedback to back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,14 +1,17 @@
 import { Pressable } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigation } from "expo-router";
 import { ArrowLeft } from "iconsax-react-native";
 
 const BackButton = () => {
+  const [isPressed, setPressed] = useState(false);
   const navigation = useNavigation();
   return (
     <Pressable
       className="rounded-2xl p-3"
-      style={{ backgroundColor: "#ffffff88" }}
+      onPressIn={() => setPressed(true)}
+      onPressOut={() => setPressed(false)}
+      style={{ backgroundColor: "#ffffff88", opacity: isPressed ? 0.6 : 1 }}
       onPress={() => navigation.goBack()}
     >
       <ArrowLeft size="32" color="#000000" />
