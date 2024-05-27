27cde0b334e9b4dbea2d67ba4ac0f60ab8cf949b
Isaac Hussain <isaaclearns58@gmail.com>
2024-05-27 15:00:31 +0300

    refactor: update fonts in UIButton

--- src/components/ui/UIButton.tsx
+++ src/components/ui/UIButton.tsx
@@ -1,5 +1,5 @@
 import { Pressable, PressableProps, Text, View } from "react-native";
-import React, { ReactNode, forwardRef } from "react";
+import React, { ReactNode, forwardRef, useState } from "react";
 import { VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/utils/cn";
 
@@ -23,7 +23,7 @@ const buttonVariants = {
     fill: ["bg-primary-100"],
   },
   size: {
-    small: [""],
+    small: ["px-3 py-1"],
     default: ["py-2 px-4"],
     large: ["py-4 items-center flex-1 mx-4"],
   },
@@ -49,18 +49,26 @@ const UIButton = forwardRef<View, UIButtonProps>(
       ...props
     } = Props;
 
+    const [isPressed, setPressed] = useState(false);
+
     const textKey = `${size}Text` as keyof typeof buttonVariants.text;
     const textStyle = buttonVariants.text[textKey];
+
+    /* const onBtnPress = () => setPressed(true); */
 
     return (
       <View className={cn("flex-row", containerStyles)}>
         <Pressable
           ref={forwardedRef}
+          onPressIn={() => setPressed(true)}
+          onPressOut={() => setPressed(false)}
           className={cn(
             btnStyles({ variant: variant, size: size }),
             buttonStyles,
+            isPressed ? "opacity-60" : "",
           )}
           {...props}
+          /* onPress={onBtnPress} */
         >
           <Text
             className={cn(buttonVariants.text.baseText, textStyle, textStyles)}
