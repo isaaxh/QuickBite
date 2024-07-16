ab9c097dd83435b8061bac137de0b1a824ae68ad
Isaac Hussain <isaaclearns58@gmail.com>
2024-07-16 12:16:39 +0300

    feat: add persistance to firebase

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -14,5 +15,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
