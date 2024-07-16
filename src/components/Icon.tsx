3f961013f0f583820c522b628d901fd6aad28295
Isaac Hussain <isaaclearns58@gmail.com>
2024-07-16 12:24:04 +0300

    refactor: add proptypes

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome";
 
-export default function Icon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
-  color: string;
-  size: React.ComponentProps<typeof FontAwesome>["size"];
-}) {
-  return <FontAwesome {...props} />;
+export type TIconProps = {
+  name: React.ComponentProps<typeof FontAwesome6>["name"];
+  color: React.ComponentProps<typeof FontAwesome6>["color"];
+  size: React.ComponentProps<typeof FontAwesome6>["size"];
+};
+
+export default function Icon(props: TIconProps) {
+  return <FontAwesome6 {...props} />;
 }
